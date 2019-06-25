2ca56a6fb92b23f8fbbc3498393d65d90d5f2624
Anna Spysz <anna.spysz@stackery.io>
2019-06-25 16:27:53 -0700

    Added function code

--- src/getItems/index.js
+++ src/getItems/index.js
@@ -1,6 +1,22 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
 
-  return {};
+exports.handler = async (event, context) => {
+  // Get items from the ItemTable
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const params = {
+    TableName: process.env.TABLE_NAME
+  };
+
+  try {
+    items = await dynamodb.scan(params).promise();
+    console.log(`Getting data from table ${process.env.TABLE_NAME}.`);
+  } catch (error) {
+    console.log(`Error getting data from table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
+    throw new Error(error); // stop execution if data from dynamodb not available
+  }
+
+  return {
+    itemCount: items.length,
+    items: items
+  };
 };
--- src/postItem/index.js
+++ src/postItem/index.js
@@ -1,6 +1,34 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
 
-  return {};
+exports.handler = async (event, context) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const params = {
+    TableName: process.env.TABLE_NAME, // get the table name from the automatically populated environment variables
+    Item: {
+      id: '1', // modify with each invoke so the id does not repeat
+      content: 'This is my content' // modify content here
+    },
+    ConditionExpression: 'attribute_not_exists(id)', // do not overwrite existing entries
+    ReturnConsumedCapacity: 'TOTAL'
+  };
+
+  try {
+    // Write a new item to the ItemTable
+    await dynamodb.put(params).promise();
+    console.log(`Writing item ${params.Item.id} to table ${process.env.TABLE_NAME}.`);
+  } catch (error) {
+    console.log(`Error writing to table ${process.env.TABLE_NAME}. Make sure this function is running in the same environment as the table.`);
+    throw new Error(error); // stop execution if dynamodb is not available
+  }
+  
+  // Return a 200 response if no errors
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: 'Success!'
+  };
+
+  return response;
 };
